944032a742ef595f33427387e9cc87785cd5fc99
ya960111 <ya960111@naver.com>
2024-03-26 23:06:36 +0900

    함수화하기 / 숙제 : SetObject 구현해보기

--- 01. Grammar/34.galag.cpp
+++ 01. Grammar/34.galag.cpp
@@ -32,6 +32,84 @@ void SetOption()
 	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &CursorInfo);
 }
 
+void SetBoard(Object* pTarget)
+{
+	for (size_t i = 0; i < 5; i++)
+	{
+		board[pTarget->iPosY][pTarget->iPosX + i] = pTarget->arrImage[i];
+	}
+}
+
+void Input(Object* pPlayer)
+{
+	// 입력
+		// cout cin
+	if (_kbhit() != 0)
+	{
+		switch (_getch())
+		{
+		case 'W':
+		case 'w':
+			pPlayer->iPosY -= 1;
+			break;
+		case 'S':
+		case 's':
+			pPlayer->iPosY += 1;
+			break;
+		case 'A':
+		case 'a':
+			pPlayer->iPosX -= 1;
+			break;
+		case 'D':
+		case 'd':
+			pPlayer->iPosX += 1;
+			break;
+		default:
+			break;
+		}
+	}
+
+	int playerImageLength = strlen(pPlayer->arrImage);// 문자열 길이
+
+	if (pPlayer->iPosX < 0) // x < 0 왼쪽 점점 감소
+	{
+		pPlayer->iPosX = 0;
+	}
+	else if (pPlayer->iPosY < 0)
+	{
+		pPlayer->iPosY = 0;
+	}
+	else if (pPlayer->iPosY > BOARD_Y - 1)
+	{
+		pPlayer->iPosY = BOARD_Y - 1;
+	}
+	else if (pPlayer->iPosX > BOARD_X - playerImageLength)
+	{
+		pPlayer->iPosX = BOARD_X - playerImageLength;
+	}
+}
+
+void RenderBoard()
+{
+	for (size_t i = 0; i < BOARD_Y; i++)
+	{
+		for (size_t j = 0; j < BOARD_X; j++)
+		{
+			cout << board[i][j];
+		}
+		cout << endl;
+	}
+}
+void ClearBoard()
+{
+	for (size_t i = 0; i < BOARD_Y; i++)
+	{
+		for (size_t j = 0; j < BOARD_X; j++)
+		{
+			board[i][j] = ' ';
+		}
+	}
+}
 //반환형 함수이름 (인자)
 	//{ 구현부}
 	/*
@@ -62,92 +140,39 @@ void SetOption()
 
 int main()
 {
+	Object airport;
+	airport.iPosX = 15;
+	airport.iPosY = 15;
+	strcpy(airport.arrImage, "<0>");
+	airport.size = strlen(airport.arrImage); // 값 넣음 
+	// strlen : 문자 갯수
+
+
+
 	SetOption();
 
 	Object player;
 	SetObject(&player, 15, 15, "<-0->");
-	
+
+
+
 	Object monster;
 	SetObject(&monster, 15, 10, "<0-0>");
 	
 	while (true)
 	{
 		system("cls");
-
-		// 입력
-		// cout cin
-
-		if (_kbhit() != 0)
-		{
-			switch (_getch())
-			{
-			case 'W':
-			case 'w':
-				player.iPosY -= 1;
-				break;
-			case 'S':
-			case 's':
-				player.iPosY += 1;
-				break;
-			case 'A':
-			case 'a':
-				player.iPosX -= 1;
-				break;
-			case 'D':
-			case 'd':
-				player.iPosX += 1;
-				break;
-			default:
-				break;
-			}
-		}
-
-		int playerImageLength = strlen(player.arrImage);// 문자열 길이
-
-		if (player.iPosX < 0) // x < 0 왼쪽 점점 감소
-		{
-			player.iPosX = 0;
-		}
-		else if (player.iPosY < 0)
-		{
-			player.iPosY = 0;
-		}
-		else if (player.iPosY > BOARD_Y - 1)
-		{
-			player.iPosY = BOARD_Y - 1;
-		}
-		else if (player.iPosX > BOARD_X - playerImageLength)
-		{
-			player.iPosX = BOARD_X - playerImageLength;
-		}
-		//오른쪽 점점 증가 
 		
-		for (size_t i = 0; i < 5; i++)
-		{
-			board[player.iPosY][player.iPosX + i] = player.arrImage[i];
-		}
-		
-		for (size_t i = 0; i < 5; i++)
-		{
-			board[monster.iPosY][monster.iPosX + i] = monster.arrImage[i];
-		}
-		
-		for (size_t i = 0; i < BOARD_Y; i++)
-		{
-			for (size_t j = 0; j < BOARD_X; j++)
-			{
-				cout << board[i][j];
-			}
-			cout << endl;
-		}
+		// 입력 및 플레이어 이동
+		Input(&player); 
 
-		for (size_t i = 0; i < BOARD_Y; i++)
-		{
-			for (size_t j = 0; j < BOARD_X; j++)
-			{
-				board[i][j] = ' ';
-			}
-		}
+		// 보드에 이미지 그리기
+		SetBoard(&player);
+		SetBoard(&monster); 
+		
+		RenderBoard();
+		ClearBoard();
+		
 
 		Sleep(20); // 프로그램 쉬기 20 / 1000
 	}
@@ -156,12 +181,21 @@ int main()
 }
 
 /*
+03/26 숙제
+1. SetObject만들기
+-> 함수로 값 세팅해보기
+단, 함수를 보지 않고 만들어보기
+
+2. cout으로 Object 변수에 저장된 값 모두 출력하기
+*/
+
+/*
 03/23 숙제
 1. Object 구조체에 size 변수를 추가하기
   - 플레이어 사이즈 값 넣기 *strlen 활용 
   - 몬스터 사이즈 값 넣기 *strlen 활용
 
-2. while문 안에 코드들 함수화하기(복습)
+2. while문 안에 코드들 함수화하기(복습)(클리어)
   - 입력 코드 함수화 하기
   - board에 플레이어와 몬스터 넣는 코드 함수화 하기
   - board 출력하는 코드 함수화하기
@@ -201,3 +235,21 @@ int main()
  - 원하는 방향으로 계속해서 이동한다.
  - 전투기와 부딪히거나, 게임판 밖으로 나가면 삭제
 */
+
+/*
+구조체 Object
+
+1. 좌표 x,y
+2. 이미지를 저장할 문자열 
+3. 이미지를 저장한 문자열 길이
+
+Object에 값 넣기
+
+Object 변수 선언
+좌표 x 20 y 30
+이미지 "<^0^>" strcpy
+이미지 길이 
+int a = 함수
+*/
+
+
